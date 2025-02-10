c44450cf43ac143621ad9d37c139d483ac3d07e3
Marco Aurélio Guerra Pereira <marcoaurelioguerrap@gmail.com>
2025-02-10 10:36:33 -0300

    Final commit week 2

--- week 02/week 02.R
+++ week 02/week 02.R
@@ -1,6 +1,55 @@
 library(ggplot2)
+library(showtext)
+font_add_google("Teko")
+showtext_auto()
 
 # 1 - Load data ----
 
-data <- read.csv("week 02/data.csv")
+data <- read.csv("week 02/data.csv",row.names = NULL )
 
+# 2 - Adding index to data
+
+data$index <- nrow(data):1
+
+
+# 3 - Plot data ----
+
+ggplot(data, aes(x=index, y = Land)) +
+  geom_col(
+    fill = "#e02d47", 
+    width = .5
+  ) +
+  geom_text(
+    aes(
+      x=index, y=0,
+      label=Date
+    ),
+    family = "Teko",
+    hjust= 1.1
+  ) +
+  geom_text(
+    aes(
+      x=index,
+      y=Land/2,
+      label=ifelse(Date %in% c(1874,1899), scales::comma(Land),"") 
+    ),
+    family = "Teko",
+    size=6,
+  ) +
+  coord_flip() +
+  labs(title = "ACRES OF LAND OWNED BY NEGROES\nIN GEORGIA.", 
+       y = NULL,
+       x = NULL
+  ) +
+  scale_y_continuous(labels = scales::comma) + 
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "#e6d6c6ff"), 
+    plot.title = element_text(
+      family = "Teko",
+      size = 24,
+      hjust = 0.5,
+      margin = margin(t = -10, b = -20) 
+    ),
+    plot.margin = margin(t = 20, r = 20, b = 20, l = 20)
+  )
